41f0e1bb211b77339f9dfc0235b71332fef79932
denisunderonov <denisunderonov2@gmail.com>
2026-01-12 01:31:20 +0300

    Добавил задание 2

---
+++ task1.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+var denis string = "Denis"
+var year int = 2026
+
+func main() {
+	fmt.Println("Hello, my name is", denis, "and i started learning go in", year)
+}
---
+++ task2.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	if age:= 18; age >= 18 {
+		fmt.Println("Access granted")
+	} else {
+		fmt.Println("Access denied")
+	}
+}
